bb6e218c64eec46f0d8419822f87a94fd243c893
Ricardo Mejolaro <ricardo.mejolaro@gmail.com>
2021-11-18 20:00:07 -0300

    global styles included

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
 
 export function App() {
   return (
     <div className="App">
       <h1>Iniciando o Projeto</h1>
+      <GlobalStyle />
     </div>
   );
 }
